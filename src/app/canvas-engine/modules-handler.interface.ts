22cb19756d0df55a0d693603b3dc7afb68f94f65
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 11:15:29 +0530

    Project restructure

--- src/app/canvas-engine/modules-handler.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ICanvasModule, ICanvasModuleRegistry } from "../modules/module.interface";
-import { ICanvasEngineEventHandler } from "./event-handler.interface";
-
-
-export interface ICanvasEngineModulesHandler {
-        
-    getModules: () => ICanvasModule[];
-
-}
