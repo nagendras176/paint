cd70b25b3da2c1373de4ad56431ea38891dc538b
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 22:30:00 +0530

    Add eraser component

---
+++ src/app/modules/modules/eraser/eraser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eraser',
+  standalone: true,
+  imports: [],
+  templateUrl: './eraser.component.html',
+  styleUrl: './eraser.component.scss'
+})
+export class EraserComponent {
+
+}
