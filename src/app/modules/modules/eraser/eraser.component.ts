06b561183210d9cde77efdf90145848dd5060639
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 23:57:58 +0530

    Add imports and functionality for erasing on canvas

--- src/app/modules/modules/eraser/eraser.component.ts
+++ src/app/modules/modules/eraser/eraser.component.ts
@@ -1,10 +1,13 @@
 import { Component , OnInit} from '@angular/core';
 import { ICanvasModule } from '../../module.interface';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { EngineService } from '../../../engine/engine.service';
 
 @Component({
   selector: 'app-eraser',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,MatButtonModule],
   templateUrl: './eraser.component.html',
   styleUrl: './eraser.component.scss'
 })
@@ -16,7 +19,9 @@ export class EraserComponent implements OnInit, ICanvasModule{
 
    private _stop: boolean = true;
    
-   constructor(){}
+   constructor(private engine: EngineService){}
+
+    private _size: number = 10;
 
     ngOnInit(): void {
         
@@ -24,11 +29,67 @@ export class EraserComponent implements OnInit, ICanvasModule{
 
 
     start(): void {
-        
+      this._stop = false;
+      const events = this.engine.getEventHandler();
+      const canvasContext = this.engine.getCanvasContext();
+
+      this.preErase(events, canvasContext);
+    }
+
+    private currentX: number = 0;
+    private currentY: number = 0;
+    private isErasing: boolean = false;
+
+    preErase(events: any, canvasContext: any): void {
+      const mouseDownSubscription = events.onMouseDown.subscribe((event: MouseEvent) => {
+        if(this._stop){
+           mouseDownSubscription.unsubscribe();
+            return;
+        }
+        this.isErasing = true;
+        mouseDownSubscription.unsubscribe();
+        this.startErasing(events, canvasContext);
+   });
+    }
+
+    startErasing(events: any, canvasContext: any): void {
+      const mouseMoveSubscription = events.onMouseMove.subscribe((event: MouseEvent) => {
+        if(this._stop){
+            mouseMoveSubscription.unsubscribe();
+            return;
+        }
+        if(this.isErasing){
+            this.erase(canvasContext);
+            this.currentX = event.clientX;
+            this.currentY = event.clientY;
+        }
+
+    });
+    this.postErase(events);
+    }
+
+    postErase(events: any): void {
+      const mouseUpSubscription = events.onMouseUp.subscribe((event: MouseEvent) => {
+        if(this._stop){
+            mouseUpSubscription.unsubscribe();
+            return;
+        }
+        this.isErasing = false;
+        mouseUpSubscription.unsubscribe();
+        this.preErase(events, this.engine.getCanvasContext());
+    });
+    }
+
+    erase(context: CanvasRenderingContext2D): void {
+        context.clearRect(this.currentX, this.currentY, this._size, this._size);
     }
 
 
     stop(): void {
         this._stop = true;
+    }
+
+    public onClick(event: MouseEvent){
+        this.engine.notifyStart(this.id);
     }
 }
