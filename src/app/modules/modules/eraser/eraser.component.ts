530fc4cb4eb764833a4c159f0c0ea058034a0baf
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 22:32:44 +0530

    Add OnInit and ICanvasModule interfaces to EraserComponent

--- src/app/modules/modules/eraser/eraser.component.ts
+++ src/app/modules/modules/eraser/eraser.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { ICanvasModule } from '../../module.interface';
 
 @Component({
   selector: 'app-eraser',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './eraser.component.html',
   styleUrl: './eraser.component.scss'
 })
-export class EraserComponent {
+export class EraserComponent implements OnInit, ICanvasModule{
 
+   public static id = 'eraser-module';
+
+   public id: string = EraserComponent.id;
+
+   private _stop: boolean = true;
+   
+   constructor(){}
+
+    ngOnInit(): void {
+        
+    }
+
+
+    start(): void {
+        
+    }
+
+
+    stop(): void {
+        this._stop = true;
+    }
 }
