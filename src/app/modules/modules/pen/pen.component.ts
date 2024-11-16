fa753bf2cdd007766f5af6be5ad5cecfb51b9971
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 22:25:19 +0530

    Update pen component with color picker and size slider

--- src/app/modules/modules/pen/pen.component.ts
+++ src/app/modules/modules/pen/pen.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ICanvasModule } from '../../module.interface';
 import { EngineService } from '../../../engine/engine.service';
-import { ModuleRegistry } from '../../module-registry';
+import {MatSliderModule} from '@angular/material/slider';
+import {
+    MatDialog,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogRef,
+    MatDialogTitle,
+  } from '@angular/material/dialog';
+import { ColorPickerComponent } from '../../../commons/components/color-picker/color-picker.component';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @Component({
@@ -22,6 +35,11 @@ export class PenComponent implements ICanvasModule{
      constructor(private engine: EngineService){}
 
      public id: string = PenComponent.id;
+
+     private _penColor: string = 'black';
+     private _penSize: number = 2;
+
+     readonly dialog = inject(MatDialog);
 
      
 
@@ -55,8 +73,8 @@ export class PenComponent implements ICanvasModule{
           context.beginPath();
           context.moveTo(startX, startY);
           context.lineTo(endX, endY);
-          context.strokeStyle = 'black';
-          context.lineWidth = this.weight;
+          context.strokeStyle = this._penColor;
+          context.lineWidth = this._penSize;
           context.stroke();
           context.closePath();
       }
@@ -107,6 +125,88 @@ export class PenComponent implements ICanvasModule{
         });
       }
 
+      public onClick(event: MouseEvent){
+        this.engine.notifyStart(this.id);
+        const dialogRef: MatDialogRef<PenConfigDialog>=this.dialog.open(PenConfigDialog);
+        dialogRef.afterClosed().subscribe((result) => {
+            if(result){
+                this._penColor = result.color;
+                this._penSize = result.size;
+            }
+        });
+  }
+
 
 }
+
+
+
+@Component({
+    selector: 'pen-config-dialog',
+    templateUrl: './pen.config.dialog.html',
+    styleUrl: './pen.config.dialog.scss',
+    standalone: true,
+    imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, 
+      ColorPickerComponent, MatIconModule, MatSliderModule,FormsModule, MatInputModule, MatFormFieldModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  export class PenConfigDialog implements OnInit{
+    readonly dialogRef = inject(MatDialogRef<PenConfigDialog>);
+
+    constructor(){}
+
+    ngOnInit(): void {
+       
+    }
+
+    public value: number = 1;
+    public get size(){
+        return (Number(this.value)/100)*25+1;
+    }
+
+    public get colorSet(): number[][]{
+        return [
+            [0, 0, 0],
+            [255, 0, 0],
+            [0, 255, 0],
+            [0, 0, 255],
+            [255, 255, 0],
+            [255, 0, 255],
+            [0, 255, 255],
+            [255, 255, 255]
+        ]
+    }
+
+    public getColor(rgb: number[]){
+        return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    }
+
+    private rgbStringToNumber(rgb: string){
+        const values = rgb.split(',');
+        return values.map((value) => Number(value));
+    }
+
+
+
+    public r=0
+    public g=0
+    public b=0;
+
+    public setColor(rgb: number[]){
+        this.r = rgb[0];
+        this.g = rgb[1];
+        this.b = rgb[2];
+    }
+
+    public onCancel(){
+        this.dialogRef.close();
+    }
+
+    public onPick(){
+        this.dialogRef.close({color: this.getColor([this.r,this.g,this.b]), size: this.size});
+    }
+
+
+  }
+  
 
