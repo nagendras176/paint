9ff85888806223eaae74064bdc64906a3f255269
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 11:51:31 +0530

    Refactor module registration and add default module support

--- src/app/modules/module-registry.ts
+++ src/app/modules/module-registry.ts
@@ -1,20 +1,20 @@
-import { ICanvasModule } from "./module.interface";
-import { EngineService } from "../engine/engine.service";
+import { ICanvasModule, ICanvasModuleClass } from "./module.interface";
 
-
-export interface ICanvasModuleClass {
-    readonly id: string; 
-    new (engine: EngineService): ICanvasModule; 
-  }
-  
 
 
 export class ModuleRegistry {
     
     private static _modules: ICanvasModuleClass[] = [];
+    private static _defaultModuleId?: string;
 
-    public static registerModule(module: ICanvasModuleClass): void {
+    public static registerModule(module: ICanvasModuleClass, options: {default: boolean}): void {
         ModuleRegistry._modules.push(module);
+        if(options.default) {
+            if(this._defaultModuleId) {
+                throw new Error('Default module already set');
+            }
+            this._defaultModuleId = module.id;
+        }
     }
 
     public static getModules(): ICanvasModuleClass[] {
@@ -23,6 +23,10 @@ export class ModuleRegistry {
 
     public static getModuleById(id: string): ICanvasModuleClass | undefined {
         return ModuleRegistry._modules.find(module => module.id === id);
+    }
+
+    public static getDefaultModuleId(): string {
+        return ModuleRegistry._defaultModuleId as string;
     }
 
 }
--- src/app/modules/module.interface.ts
+++ src/app/modules/module.interface.ts
@@ -1,7 +1,15 @@
 import { Directive } from "@angular/core";
+import { EngineService } from "../engine/engine.service";
 
 export interface ICanvasModule extends Directive{
     id: string;
     start(): void;
     stop(): void;
 }
+
+
+
+export interface ICanvasModuleClass {
+    readonly id: string; 
+    new (engine: EngineService): ICanvasModule; 
+  }
