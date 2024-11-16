5ee7b906182b36deffbc2bbd452819acfe72533c
Nagendra <40751501+nagendras176@users.noreply.github.com>
2024-11-16 19:58:48 +0530

    Update paint component HTML, SCSS, and TS files

--- src/app/paint/paint.component.ts
+++ src/app/paint/paint.component.ts
@@ -1,13 +1,14 @@
-import { Component, ElementRef, OnInit, Signal, viewChild, Renderer2, createComponent } from '@angular/core';
+import { Component, ElementRef, OnInit, Signal, viewChild } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { ICanvasModule } from '../modules/module.interface';
+import { ICanvasModule, ICanvasModuleClass } from '../modules/module.interface';
 import { ModulesHolderComponent } from '../modules-holder/modules-holder.component';
 import { EngineService } from '../engine/engine.service';
+import { ModuleHolderComponent } from '../modules-holder/module-holder/module-holder.component';
 @Component({
   selector: 'app-paint',
   standalone: true,
-  imports: [MatButtonModule,MatIconModule,ModulesHolderComponent],
+  imports: [MatButtonModule,MatIconModule,ModulesHolderComponent, ModuleHolderComponent],
   templateUrl: './paint.component.html',
   styleUrl: './paint.component.scss',
   providers: [EngineService]
@@ -15,6 +16,7 @@ import { EngineService } from '../engine/engine.service';
 export class PaintComponent implements OnInit {
 
   private canvasSignalElement: Signal<ElementRef<HTMLCanvasElement>> = viewChild.required<ElementRef<HTMLCanvasElement>>('canvasBoard');
+  public modules: ICanvasModuleClass[] = [];
 
 
   constructor(private engine: EngineService) { }
@@ -24,7 +26,10 @@ export class PaintComponent implements OnInit {
     this.setUpCanvasWindow(canvasElementRef);
     const canvasElement: HTMLCanvasElement = this.getCanvasElement(canvasElementRef);
     this.engine.startEngine(canvasElement);
+    this.modules = this.engine.getModules();
+    this.startModules();
   }
+
 
   private getCanvasElementRef(): ElementRef<HTMLCanvasElement> {
        const canvasElementRef: ElementRef<HTMLCanvasElement> = this.canvasSignalElement();
@@ -32,20 +37,48 @@ export class PaintComponent implements OnInit {
   }
 
   private setUpCanvasWindow(canvasElement: ElementRef<HTMLCanvasElement>): void {
+    const resizeCanvas = () => {
       const parentElement: HTMLElement = canvasElement.nativeElement.parentElement as HTMLElement;
-      const parentElementWidth: number = parentElement.clientWidth as number;
-      const parentElementHeight: number = parentElement.clientHeight as number;
+  
+      if (!parentElement) {
+        console.error("Parent element not found for canvas.");
+        return;
+      }
+  
+     
+      const parentElementWidth: number = parentElement.clientWidth;
+      const parentElementHeight: number = parentElement.clientHeight;
+  
 
-      const pixelRatio: number = window.devicePixelRatio as number;
-
+      const pixelRatio: number = 1;
+  
+    
       canvasElement.nativeElement.width = parentElementWidth * pixelRatio;
       canvasElement.nativeElement.height = parentElementHeight * pixelRatio;
-      
+  
+     
+      const context = canvasElement.nativeElement.getContext('2d');
+      if (context) {
+        context.scale(pixelRatio, pixelRatio);
+      }
+    };
+  
+    resizeCanvas();
+  
+    window.addEventListener('resize', resizeCanvas);
+  
+   
   }
 
   private getCanvasElement(elemRef: ElementRef<HTMLCanvasElement>): HTMLCanvasElement {
         const canvasElement: HTMLCanvasElement = this.getCanvasElementRef().nativeElement as HTMLCanvasElement;
         return canvasElement;
+  }
+
+  private startModules(): void {
+      setTimeout(() => {
+          this.engine.startDefaultModule();
+      },100);
   }
 
 
